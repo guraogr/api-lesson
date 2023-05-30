3d1cf67d146cc38016ee4976a4563260ecfab312
guraogr <guraogr@gmail.com>
2023-05-31 01:33:58 +0900

    feat: 基本処理の雛形作成

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import * as bcrypt from 'bcrypt';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { Msg } from './interfaces/auth.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwt: JwtService,
+    private readonly config: ConfigService,
+  ) {}
+
+  async signUp(dto: AuthDto): Promise<Msg> {
+    const hashed = await bcrypt.hash(dto.password, 12);
+
+    try {
+      await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hashedPassword: hashed,
+        },
+      });
+      return {
+        message: 'ok',
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('This email is already taken');
+        }
+      }
+      throw error;
+    }
+  }
+}
