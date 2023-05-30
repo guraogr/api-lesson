abb3b8e09ecb23c8bd1559bae9dde0685fe5f83b
guraogr <guraogr@gmail.com>
2023-05-31 01:53:13 +0900

    feat: auth処理を作成中

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,8 +4,8 @@ import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto/auth.dto';
 import * as bcrypt from 'bcrypt';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
-import { Msg } from './interfaces/auth.interface';
+import { Prisma } from '@prisma/client';
+import { Jwt, Msg } from './interfaces/auth.interface';
 
 @Injectable()
 export class AuthService {
@@ -29,12 +29,39 @@ export class AuthService {
         message: 'ok',
       };
     } catch (error) {
-      if (error instanceof PrismaClientKnownRequestError) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
           throw new ForbiddenException('This email is already taken');
         }
       }
       throw error;
     }
+  }
+
+  async login(dto: AuthDto): Promise<Jwt> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) throw new ForbiddenException('Email or password incorrect');
+    const isValid = await bcrypt.compare(dto.password, user.hashedPassword);
+    if (!isValid) throw new ForbiddenException('Email or password incorrect');
+    return this.generateJwt(user.id, user.email);
+  }
+
+  async generateJwt(userId: number, email: string): Promise<Jwt> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '5m',
+      secret: secret,
+    });
+    return {
+      accessToken: token,
+    };
   }
 }
